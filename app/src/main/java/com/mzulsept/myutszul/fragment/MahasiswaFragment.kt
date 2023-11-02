89939ae84b2d11c4bfe6ec2517e4c73484a2b8c2
mzulseptriawan <mzulfikarseptriawan@gmail.com>
2023-11-02 11:58:31 +0700

    fix swipe refresh & soal no 1

--- app/src/main/java/com/mzulsept/myutszul/fragment/MahasiswaFragment.kt
+++ app/src/main/java/com/mzulsept/myutszul/fragment/MahasiswaFragment.kt
@@ -19,30 +19,21 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MahasiswaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MahasiswaFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
-    // TODO: Rename and change types of parameters
     private lateinit var adapter: MahasiswaAdapter
     private lateinit var recyclerView: RecyclerView
     private lateinit var mhsArrayList: ArrayList<DataItem>
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_mahasiswa, container, false)
-        onRefresh()
+        val view = inflater.inflate(R.layout.fragment_mahasiswa, container, false)
+        swipeRefreshLayout = view.findViewById(R.id.swiperefresh)
+        swipeRefreshLayout.setOnRefreshListener(this)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -53,35 +44,67 @@ class MahasiswaFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
         recyclerView.layoutManager = layoutManager
         recyclerView.setHasFixedSize(true)
 
-//        mendapatkan instance dari Api Service dari ApiConfig
+        // mendapatkan instance dari Api Service dari ApiConfig
         val apiService = ApiConfig.getService()
-//        melakukan pengadilan Api untuk mendapatkan data ResponPost
+        // melakukan pengadilan Api untuk mendapatkan data ResponPost
         val call = apiService.getMhs()
 
         call.enqueue(object : Callback<ResponseMahasiswa> {
             override fun onResponse(call: Call<ResponseMahasiswa>, response: Response<ResponseMahasiswa>) {
                 if (response.isSuccessful){
                     val responseMahasiswa = response.body()
-
                     mhsArrayList = responseMahasiswa?.data as ArrayList<DataItem>
                     recyclerView.adapter = MahasiswaAdapter(mhsArrayList) { selectedItem ->
-
+                        // Implementasi aksi item click di sini jika diperlukan
                     }
                 } else {
                     // handle kesalahan respons
                     Log.e("MahasiswaFragment", "Error: ${response.message()}")
                 }
+                // Selesaikan animasi refresh setelah mendapatkan respons
+                swipeRefreshLayout.isRefreshing = false
             }
 
             override fun onFailure(call: Call<ResponseMahasiswa>, t: Throwable) {
                 Log.e("MahasiswaFragment", "Failed: ${t.message}")
-
+                // Selesaikan animasi refresh jika panggilan gagal
+                swipeRefreshLayout.isRefreshing = false
             }
-
         })
     }
 
     override fun onRefresh() {
+        // Implementasikan aksi refresh di sini, misalnya memuat ulang data dari server
+        // Panggil metode untuk memuat data kembali
+        loadData()
+    }
 
+    private fun loadData() {
+        // Mendapatkan instance dari Api Service dari ApiConfig
+        val apiService = ApiConfig.getService()
+        // Melakukan pengadilan Api untuk mendapatkan data ResponPost
+        val call = apiService.getMhs()
+
+        call.enqueue(object : Callback<ResponseMahasiswa> {
+            override fun onResponse(call: Call<ResponseMahasiswa>, response: Response<ResponseMahasiswa>) {
+                if (response.isSuccessful){
+                    val responseMahasiswa = response.body()
+                    mhsArrayList.clear()
+                    mhsArrayList.addAll(responseMahasiswa?.data as ArrayList<DataItem>)
+//                    adapter.notifyDataSetChanged()
+                } else {
+                    // handle kesalahan respons
+                    Log.e("MahasiswaFragment", "Error: ${response.message()}")
+                }
+                // Selesaikan animasi refresh setelah mendapatkan respons
+                swipeRefreshLayout.isRefreshing = false
+            }
+
+            override fun onFailure(call: Call<ResponseMahasiswa>, t: Throwable) {
+                Log.e("MahasiswaFragment", "Failed: ${t.message}")
+                // Selesaikan animasi refresh jika panggilan gagal
+                swipeRefreshLayout.isRefreshing = false
+            }
+        })
     }
 }
